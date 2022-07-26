c4d938f1d7e9154e622c48b4fe5424def83c7637
Harshit Harsh <104749996+hharsh05@users.noreply.github.com>
2022-07-26 12:57:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@
 
 
 import streamlit as st
-st.title("Image Classification with Google's Teachable Machine")
-st.header("Brain Tumor MRI Classification Example")
-st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 import keras
 from PIL import Image, ImageOps
 import tensorflow as tf
 import numpy as np
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Brain Tumor MRI Classification Example")
+st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 from img_classification import teachable_machine_classification
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
 if uploaded_file is not None:
